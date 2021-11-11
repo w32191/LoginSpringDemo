7cde94b21d1f796cb10a3e8d0642e240952825c3
王柏翔 <w32191@gmail.com>
2021-11-11 13:16:08 +0800

    After

--- src/main/java/com/example/loginspringdemo/aspect/MyAspect.java
+++ src/main/java/com/example/loginspringdemo/aspect/MyAspect.java
@@ -1,5 +1,6 @@
 package com.example.loginspringdemo.aspect;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -9,7 +10,12 @@ import org.springframework.stereotype.Component;
 public class MyAspect {
 
   @Before("execution(* com.example.loginspringdemo.javabean.HpPrinter.*(..))")
-  public void before(){
+  public void before() {
     System.out.println("I'm before");
+  }
+
+  @After("execution(* com.example.loginspringdemo.javabean.HpPrinter.*(..))")
+  public void after() {
+    System.out.println("I'm after.");
   }
 }
